bbc4ceeacd56a6e93edd6e857f121f1269509577
Saransh Sagar <saransh8@live.com>
2020-06-22 16:53:20 +0100

    added clear history display functionality

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function display(num) {
   document.getElementById("display").innerText += num;
 }
 
-function clearDisplay() {
-  document.getElementById("display").innerText = "";
-}
-
 function calculate() {
   let cal = document.getElementById("display").innerText;
   document.getElementById("history").innerText = cal;
   let calculated = eval(cal);
   document.getElementById("display").innerText = calculated;
+}
 
+function clearDisplay() {
+  document.getElementById("display").innerText = "";
+  document.getElementById("history").innerText = "";
 }
