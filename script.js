c9088d32d62c4770087f254a3198737aa6366f42
sarsag <58093663+sarsag@users.noreply.github.com>
2020-07-06 01:14:51 +0100

    Updated to add keyboard functionality

--- script.js
+++ script.js
@@ -20,5 +20,66 @@ function backspace() {
     0,
     entry.length - 1
   );
-  console.log("bkspace");
 }
+
+document.onkeydown = function (e) {
+  switch (e.keyCode) {
+    case 48:
+      display(0);
+      break;
+    case 49:
+      display(1);
+      break;
+    case 50:
+      display(2);
+      break;
+    case 51:
+      display(3);
+      break;
+    case 52:
+      display(4);
+      break;
+    case 53:
+      display(5);
+      break;
+    case 54:
+      display(6);
+      break;
+    case 55:
+      display(7);
+      break;
+    case 56:
+      display(8);
+      break;
+    case 57:
+      display(9);
+      break;~
+  }
+  switch (e.key) {
+    case "Enter":
+      calculate();
+      break;
+    case "=":
+      calculate();
+      break;
+    case "+":
+      display("+");
+      break;
+    case "-":
+      display("-");
+      break;
+    case "x":
+      display("*");
+      break;
+    case "*":
+      display("*");
+      break;
+    case "/":
+      display("/");
+      break;
+    case "Backspace":
+      backspace();
+      break;
+  }
+};
+
